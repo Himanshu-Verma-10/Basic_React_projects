f3d5814ceae1b47f59a420d9a918da1c37509587
Amank-root <amank.root@gmail.com>
2022-10-06 15:38:00 +0530

    qrcode gen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QrGen from './Components/QrGen';
+
+function App() {
+  return (
+    <main class="sec con">
+      <section class="hello">Get Your QrCode!</section>
+      <QrGen />
+    </main>
+  );
+}
+
+export default App;
